c3ca932429fa682cbcb71f7f400b44a8484eedef
Garreto <jeffsongarreto@hotmail.com>
2024-08-19 08:19:27 -0300

    atualização lesson 10

---
+++ Turma-02/module01/lesson10.js
@@ -0,0 +1,62 @@
+// let thingList = Array()
+
+// thingList['hardware'] = Array()
+// thingList['fruits'] = Array()
+// thingList['people'] = Array('Aristóteles', 'Tolkien', 'Josefa')
+
+// thingList['hardware'][0] = 'Notebook'
+// thingList['hardware'][1] = 'Mouse'
+// thingList['hardware'][2] = 'Teclado'
+// thingList['fruits'][0] = 'Laranja'
+// thingList['fruits'][1] = 'Tangerina'
+// thingList['fruits'][2] = 'Banana'
+
+// console.log(thingList)
+
+// console.table(thingList)
+
+
+
+// let fruitsList = []
+
+// fruitsList[0] = 'Maça'
+// fruitsList[1] = 'Banana'
+// fruitsList[2] = 'Melancia'
+// fruitsList[3] = 'Morango'
+// fruitsList[4] = 'Morango'
+
+// console.log(fruitsList.sort())
+// console.log(fruitsList.indexOf('Morango'))
+// let index = fruitsList.indexOf('Morango')
+
+// if(index === -1){
+//     console.log('Elemento não existe')
+// } else {
+//     console.log(`O elemento existe e está na posição: ${index}`)
+// }
+
+
+
+// let numberList = []
+
+// numberList[0] = 10
+// numberList[1] = 12
+// numberList[2] = 0
+// numberList[3] = 16
+// numberList[4] = 7
+// numberList[5] = 
+
+
+
+function calculateLandArea(value1, value2){
+    let area = value1 * value2
+    return area
+}
+
+let width = 10
+let height = 200
+
+let area = calculateLandArea(width, height)
+
+console.log(`A área possui ${area} metros quadrados`)
+
---
+++ Turma-02/module01/tempCodeRunnerFile.js
@@ -0,0 +1,9 @@
+let numberList = []
+
+// numberList[0] = 10
+// numberList[1] = 12
+// numberList[2] = 0
+// numberList[3] = 16
+// numberList[4] = 7
+// numberList[5] = 
+
