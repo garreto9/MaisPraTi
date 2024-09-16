ccd6da0f6221d965014adb55b1c42398e10e39fe
Garreto <jeffsongarreto@hotmail.com>
2024-09-16 18:48:03 -0300

    atualizações aula 16 da aula 17 e adicionando modulo 02 com aula 01

--- Turma-02/module01/lesson16.js
+++ Turma-02/module01/lesson16.js
@@ -19,12 +19,12 @@ class DoublyLinkedList {
     add(data){
         let newNode = new Node(data)
 
-        if(this.head === null){
+        if (this.head === null){
             this.head = newNode
             this.tail = newNode
         } else {
             this.tail.next = newNode
-            newNode.prev = this.tail
+            newNode.previous = this.tail
             this.tail = newNode
         }
 
@@ -32,15 +32,15 @@ class DoublyLinkedList {
     }
 
     insertAt(data, index){
-        if(index < 0 || index > this.size){
+        if (index < 0 || index > this.size){
             return console.log('Index fora dos limites!')
         }
 
         let newNode = new Node(data)
         let current = this.head
-        
-        if(index === 0){
-            if(this.head === null){
+
+        if (index === 0) {
+            if (this.head === null){
                 this.head = newNode
                 this.tail = newNode
             } else {
@@ -48,21 +48,122 @@ class DoublyLinkedList {
                 this.head.previous = newNode
                 this.head = newNode
             }
-        } else if(index === this.size){
+        } else if (index === this.size){
             this.tail.next = newNode
             newNode.previous = this.tail
             this.tail = newNode
         } else {
-            for(let i = 0; i < index; i++){
+            for (let i = 0; i < index; i++){
                 current = current.next
             }
 
             newNode.next = current
             newNode.previous = current.previous
             current.previous.next = newNode
-
+            current.previous = newNode
         }
 
         this.size++
     }
+
+    removeAt(index){
+        if (index < 0 || index >= this.size){
+            return console.log('Index fora dos limites!')
+        }
+
+        let current = this.head
+
+        if (index === 0) {
+            this.head = this.head.next
+
+            if (this.head !== null){
+                this.head.previous = null
+            } else {
+                this.tail = null
+            }
+        } else if (index === this.size - 1) {
+            this.tail = this.tail.previous
+            this.tail.next = null
+        } else {
+            for (let i = 0; i < index; i++){
+                current = current.next
+            }
+
+            current.previous.next = current.next
+            current.next.previous = current.previous
+        }
+
+        this.size--
+
+    }
+
+    getData(index) {
+        if(index < 0 || index >= this.size){
+            return console.log('Index fora dos limites!')
+        }
+
+        let current = this.head
+
+        for(let i = 0; i < index; i++){
+            current = current.next
+        }
+
+        return current.data
+    } 
+
+    print(){
+        let current = this.head
+        let result = []
+        
+        while(current !== null){
+            result.push(current.data)
+            current = current.next
+        }
+
+        console.log(result.join(' <=> '))
+    } 
+    
+    getSize(){
+        return this.size
+    }
+
+    isEmpty(){
+        return this.size === 0
+    }
+
+    clear(){
+        this.head = null
+        this.tail = null
+        this.size = 0
+
+        console.log('Lista Limpa!')
+    }
 }
+
+let lista = new DoublyLinkedList()
+
+lista.add(10)
+lista.add(20)
+lista.add(30)
+lista.add(40)
+lista.add(50)
+
+lista.print()
+
+lista.insertAt(5, 0)
+lista.insertAt(15, 3)
+lista.insertAt(55, lista.getSize())
+lista.print()
+
+lista.removeAt(0)
+lista.removeAt(3)
+lista.removeAt(lista.getSize() - 1)
+lista.print()
+
+console.log('Elemento na posição 2: ', lista.getData(2))
+console.log('A lista está vazia?', lista.isEmpty())
+console.log('O tamanho da lista é', lista.getSize())
+
+
+lista.clear()
+console.log('A lista está vazia?', lista.isEmpty())
--- Turma-02/module01/tempCodeRunnerFile.js
+++ Turma-02/module01/tempCodeRunnerFile.js
@@ -1,9 +1,3 @@
-let numberList = []
-
-// numberList[0] = 10
-// numberList[1] = 12
-// numberList[2] = 0
-// numberList[3] = 16
-// numberList[4] = 7
-// numberList[5] = 
-
+lista.removeAt(0)
+// lista.removeAt(3)
+// lista.print()
